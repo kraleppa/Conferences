15c374c92aadb13d658d1e6b4490045d30b20338
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-08 20:34:44 +0100

    add addCity and addCountry procedures

--- Procedures.sql
+++ Procedures.sql
@@ -1,1 +1,86 @@
 --Procedures
+
+create procedure procedure_addCountry
+	@countryName varchar(50)
+as
+begin
+	set nocount on
+	begin try
+		if exists (
+			select country
+			from Country
+			where Country = @countryName
+		)
+		begin
+			;throw 52000, 'Country exists', 1
+		end
+		insert into Country (
+			Country
+		)
+		values (
+			@countryName
+		)
+	end try
+	begin catch
+		declare @errorMsg nvarchar(2048)
+			= 'Cannot add country. Error message: '
+			+ error_message();
+		;throw 52000, @errorMsg, 1
+	end catch
+end 
+go
+	
+
+create procedure procedure_addCity
+	@cityName varchar(50),
+	@countryName varchar(50)
+as
+begin
+	set nocount on
+	begin try
+		if(@cityName is null and @countryName is null) 
+			begin
+				;throw 52000,  'Podaj nazwe miasta i kraju', 1
+			end 
+		if not exists (
+			select *
+			from Country
+			where Country = @countryName
+		)
+			begin
+				exec procedure_addCountry 
+						@countryName
+			end
+		if not exists (
+			select * 
+			from City
+			where city = @cityName
+		)
+			begin
+				insert into City (
+					City,
+					CountryID
+				)
+				values (
+					@cityName,
+					(
+					select countryID
+					from Country
+					where Country = @countryName
+					)	
+				)
+			end
+	end try
+	begin catch 
+		declare @errorMsg nvarchar(2048)
+			= 'Cannot add city. Error message: '
+			+ error_message();
+		;throw 52000, @errorMsg, 1
+	end catch
+end
+go
+
+
+
+			
+				
