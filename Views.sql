85518a38ba795d2d2ee77725b5fb3d1d317abef3
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-08 12:54:13 +0100

    add view for left seats on each upcoming conference

--- Views.sql
+++ Views.sql
@@ -5,12 +5,33 @@ select c.CityID, c.City, c.CountryID, co.Country
 from City as c inner join Country as co on c.CountryID = c.countryID
 go
 
+
 CREATE VIEW view_countriesInDictionary as 
 select c.Country from Country as c
 go
+
 
 CREATE VIEW view_cancelledConferences as
 select * 
 from Conferences
 where Cancelled = 1
 go
+
+
+--liczba wolnych/zarezerwowanych miejsc na nadchodzace konferencje
+CREATE VIEW view_conferencesSeatsLeft as
+select c.ConferenceID, c.ConferenceName, c.Limit, cd.ConferenceDate, 
+	c.Limit -
+	((select isnull(sum(dr.NormalTickets), 0)
+		from DayReservation as dr 
+		where dr.ConferenceDayID = cd.ConferenceDayID)
+	+
+	(select isnull(sum(dr.StudentTickets), 0)
+		from DayReservation as dr 
+		where dr.ConferenceDayID = cd.ConferenceDayID)) as 'seats left'
+from Conferences as c 
+	inner join ConferenceDay as cd on cd.ConferenceID = c.ConferenceID
+where year(c.StartDate) > year(getdate()) and month(c.StartDate) > month(getdate()) and
+	day(c.StartDate) > day(getdate())
+
+
