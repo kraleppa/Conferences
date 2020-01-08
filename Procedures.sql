df7af60a3da8e093d7dd467900ed97aa72560ad5
Citoslav <misiek.nalepa@wp.pl>
2020-01-08 18:04:06 +0100

    addWorkshopToDictionary

--- Procedures.sql
+++ Procedures.sql
@@ -1,1 +1,18 @@
---Procedures
+create procedure procedure_addWorkshopToDictionary
+	@WorkshopName varchar(50),
+	@WorkshopDescription varchar(255),
+	@Price money
+as 
+begin 
+	set nocount on
+	insert into WorkshopDictionary (
+		WorkshopName,
+		WorkshopDescription,
+		Price
+	)
+	VALUES (
+		@WorkshopName,
+		@WorkshopDescription,
+		@Price
+	)
+end
