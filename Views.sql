763a116f3a356e21bfde1745a3760eed0c385536
Citoslav <misiek.nalepa@wp.pl>
2020-01-08 12:09:21 +0100

    cancelledWorkshops

--- Views.sql
+++ Views.sql
@@ -1,3 +1,11 @@
 --Views
 CREATE VIEW view_countriesInDictionary as 
 select c.Country from Country as c
+go
+
+CREATE VIEW view_cancelledWorkshop as
+select wd.WorkshopName, cd.ConferenceDate from Workshop as w
+inner join WorkshopDictionary as wd
+on wd.WorkshopDictionaryID = w.WorkshopDictionaryID
+inner join ConferenceDay as cd
+on cd.ConferenceDayID = w.ConferenceDayID
