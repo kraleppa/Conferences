33f2f4b3b9517b4c0078edbeb56d272bb0c038e6
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-08 12:05:40 +0100

    add view for cancelled Conferences

--- Views.sql
+++ Views.sql
@@ -1,10 +1,16 @@
 --Views
 
-create view view_CityInDictionary as
+CREATE VIEW view_CityInDictionary as
 select c.CityID, c.City, c.CountryID, co.Country
 from City as c inner join Country as co on c.CountryID = c.countryID
-
+go
 
 CREATE VIEW view_countriesInDictionary as 
 select c.Country from Country as c
+go
 
+CREATE VIEW view_cancelledConferences as
+select * 
+from Conferences
+where Cancelled = 1
+go
