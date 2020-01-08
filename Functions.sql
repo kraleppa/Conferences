4b70c226e23be94a7fd0fe605f84cdffc92806fa
Citoslav <misiek.nalepa@wp.pl>
2020-01-08 15:16:44 +0100

    participantsOfWorkshop

--- Functions.sql
+++ Functions.sql
@@ -1,5 +1,5 @@
 --Functions
-
+--zwraca liste uczestnikow kazdego dnia konferencji
 CREATE FUNCTION function_partiticipantsOfDayConference(@ConferenceDayID int)
 returns table 
 as
@@ -14,8 +14,6 @@ return(
 			on dr.DayReservationID = dp.DayReservationID
 		inner join ConferenceDay as cd
 			on cd.ConferenceDayID = dr.ConferenceDayID 
-		inner join Conferences as c
-			on c.ConferenceID = cd.ConferenceID
 	where cd.ConferenceDayID = @ConferenceDayID
 
 	union
@@ -32,8 +30,6 @@ return(
 			on dr.DayReservationID = dp.DayReservationID
 		inner join ConferenceDay as cd
 			on cd.ConferenceDayID = dr.ConferenceDayID 
-		inner join Conferences as c
-			on c.ConferenceID = cd.ConferenceID
 	where cd.ConferenceDayID = @ConferenceDayID
 
 )
@@ -53,3 +49,37 @@ create function function_workshopsDuringConference(@conf_id int)
 		where conf.Cancelled = 0 and w.Cancelled = 0 and conf.ConferenceID = @conf_id
 	)
 go
+
+--zwraca liste uczestnikow danego warsztatu
+create function function_participantsOfWorkshop(@WorkshopID int)
+	returns table	
+	as
+	return(
+		select p.FirstName, p.LastName, '' as 'Company'
+		from Person as p
+			inner join IndividualClient as ic
+				on ic.PersonID = p.PersonID
+			inner join DayParticipant as dp
+				on dp.PersonID = p.PersonID
+			inner join WorkshopParticipant as wp
+				on wp.DayParticipantID = dp.DayParticipantID
+			inner join WorkshopReservation as wr
+				on wr.WorkshopReservationID = wp.WorkshopReservationID
+			where wr.WorkshopID = @WorkshopID
+
+		union 
+		
+		select p.FirstName, p.LastName, co.CompanyName as 'Company'
+		from Person as p
+			inner join Employee as e
+				on e.PersonID = p.PersonID
+			inner join Company as co
+				on co.ClientID = e.ClientID
+			inner join DayParticipant as dp
+				on dp.PersonID = p.PersonID
+			inner join WorkshopParticipant as wp
+				on wp.DayParticipantID = dp.DayParticipantID
+			inner join WorkshopReservation as wr
+				on wr.WorkshopReservationID = wp.WorkshopReservationID
+			where wr.WorkshopID = @WorkshopID
+	)
