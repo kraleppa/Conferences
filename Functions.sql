cd57aff8dc6d4e09fdb8633085a03cdea6e2b706
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-08 14:46:30 +0100

    function showing workshops for given conference

--- Functions.sql
+++ Functions.sql
@@ -1,1 +1,15 @@
 --Functions
+
+--lista warsztatow dla konferencji
+create function function_workshopsDuringConference(@conf_id int)
+	returns table
+	as
+	return (
+		select conf.ConferenceName, cd.ConferenceDate, wd.WorkshopName, 
+			w.StartTime, w.EndTime
+		from Conferences as conf
+			inner join ConferenceDay as cd on cd.ConferenceID = conf.ConferenceID
+			inner join Workshop as w on w.ConferenceDayID = cd.ConferenceDayID
+			inner join WorkshopDictionary as wd on wd.WorkshopDictionaryID = w.WorkshopDictionaryID
+		where conf.Cancelled = 0 and w.Cancelled = 0 and conf.ConferenceID = @conf_id
+	)
