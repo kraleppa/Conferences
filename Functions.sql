15b65ca4d0a3412117280aac3854d718efffff15
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-15 12:44:01 +0100

    add view_reservationInfo and additional functions

--- Functions.sql
+++ Functions.sql
@@ -1,7 +1,7 @@
 --Functions
 
 --zwraca liste uczestnikow danego dnia konferencji
-create function function_partiticipantsOfDayConference(@ConferenceDayID int)
+create function function_participantsOfDayConference(@ConferenceDayID int)
 	returns table 
 	as
 	return(
@@ -36,7 +36,7 @@ create function function_workshopsDuringConference(@conf_id int)
 	as
 	return (
 		select conf.ConferenceName, cd.ConferenceDate, wd.WorkshopName, 
-			w.StartTime, w.EndTime, w.Price
+		wd.WorkshopDescription, w.limit, w.StartTime, w.EndTime, w.Price
 		from Conferences as conf
 			inner join ConferenceDay as cd 
 				on cd.ConferenceID = conf.ConferenceID
@@ -103,7 +103,7 @@ create function function_participantListForConference(@conf_id int)
 		select e.firstname, e.lastname, c.CompanyName as 'Company'
 		from employee as e
 			inner join Company as c
-				on c.ClinetID = e.ClientID
+				on c.ClientID = e.ClientID
 			inner join Person as p 
 				on p.PersonID = e.PersonID
 			inner join DayParticipant as dp 
@@ -123,8 +123,8 @@ create function function_topIndividualClients(@X int)
 	returns table
 	as
 	return (
-		select top @x c.ClientID, ic.FirstName, ic.LastName, 
-			count(r.ResevationID) as 'Liczba op³aconych rezerwacji'
+		select top(@x) c.ClientID, ic.FirstName, ic.LastName, 
+			count(r.ReservationID) as 'Liczba op³aconych rezerwacji'
 		from Clients as c
 			inner join IndividualClient as ic
 				on ic.ClientID = c.ClientID
@@ -144,7 +144,7 @@ create function function_topCompaniesByTickets(@x int)
 	returns table 
 	as
 	return (
-		select top @x com.CompanyName, 
+		select top(@x) com.CompanyName, 
 			sum(dr.NormalTickets) 
 			+ 
 			sum(dr.StudentTickets) 
@@ -156,7 +156,7 @@ create function function_topCompaniesByTickets(@x int)
 				on r.ClientID = cl.ClientID
 				and r.PaymentDate is not null
 			inner join DayReservation as dr 
-				on dr.ResevationID = r.ResevationID
+				on dr.ReservationID = r.ReservationID
 		group by com.CompanyName
 		order by 2 desc
 	)
@@ -168,7 +168,7 @@ create function function_topCompaniesByReservations(@x int)
 	returns table 
 	as
 	return (
-		select top @x com.CompanyName, 
+		select top(@x) com.CompanyName, 
 			count(r.ReservationID) as 'Liczba op³aconych rezerwacji'
 		from Company as com
 			inner join Clients as cl 
@@ -178,7 +178,7 @@ create function function_topCompaniesByReservations(@x int)
 				and r.PaymentDate is not null
 		group by com.CompanyName
 		order by count(r.ReservationID) desc
-)
+	)
 go
 
 
@@ -187,12 +187,12 @@ create function function_returnConferenceDay(@ConferenceID int, @Date date)
     returns int
     as
     begin
-    return (
-        select ConferenceDayID
-        from ConferenceDay
-        where ConferenceID = @ConferenceID and ConferenceDate = @Date
-    )
-    end
+		return (
+			select ConferenceDayID
+			from ConferenceDay
+			where ConferenceID = @ConferenceID and ConferenceDate = @Date
+		)
+		end
 go
 
 
@@ -201,11 +201,11 @@ create function function_returnValueOfWorkshop(@WorkshopDictionaryID int)
     returns money
     as
     begin
-    return (
-        select Price from WorkshopDictionary
-        where WorkshopDictionaryID = @WorkshopDictionaryID
-        )
-    end
+		return (
+			select Price from WorkshopDictionary
+			where WorkshopDictionaryID = @WorkshopDictionaryID
+			)
+		end
 go 
 
 
@@ -214,30 +214,143 @@ create function function_returnPersonID(@ReservationID int)
 	returns int
 	as
 	begin
+		return (
+				select p.PersonID 
+				from Clients as c
+				inner join IndividualClient as id 
+					on id.ClientID = c.ClientID
+				inner join Person as p 
+					on id.PersonID = p.PersonID
+				where (select ClientID from Reservation as r
+						where ReservationID = @ReservationID) = c.ClientID
+			)
+	end
+go
+
+--zwraca listê rezerwacji które nie zosta³y op³acone oraz czas jaki pozosta³
+--na op³acenie rezerwacji
+create function function_unpaidReservations(@ClientID int)
+	returns table
+	as
 	return (
-			select p.PersonID from Clients as c
-			inner join IndividualClient as id 
-				on id.ClientID = c.ClientID
-			inner join Person as p 
-				on id.PersonID = p.PersonID
-			where (select ClientID from Reservation as r
-					where ReservationID = @ReservationID) = c.ClientID
+		select ReservationID, 
+			DATEDIFF(day, GETDATE(), DATEADD(day, 7, ReservationDate)) as 'Days left'
+		from Reservation 
+		where ClientID = 1
+			and PaymentDate is null
+	)
+go
+
+
+--zwraca cene normalnego biletu w danej rezerwacji
+create function function_returnNormalTicketCost(@reservationID int)
+	returns int
+	as
+	begin
+		return (
+			select c.BasePrice*(1-isnull(p.Discount, 0))
+			from Conferences as c
+				left outer join Prices as p
+					on p.ConferenceID = c.ConferenceID and 
+						(
+							select ReservationDate 
+							from Reservation 
+							where ReservationID = @reservationID
+						) between p.StartDate and p.EndDate	
+			where c.ConferenceID = (
+				select distinct cd.ConferenceID
+				from DayReservation as dr
+					inner join Reservation as r 
+						on r.ReservationID = dr.ReservationID
+					inner join	ConferenceDay as cd 
+						on cd.ConferenceDayID = dr.ConferenceDayID
+				where r.ReservationID = @reservationID
+			)
 		)
 	end
 go
 
---zwraca listê rezerwacji danej firmy które nie zosta³y op³acone oraz czas jaki pozosta³
---na op³acenie rezerwacji
 
-create function function_unpaidReservation(@ClientID int)
-	returns table
+--zwraca cene studenckiego biletu dla rezerwacji
+create function function_returnStudentTicketCost(@reservationID int)
+	returns int
 	as
 	begin
-	return (
-		select ReservationID, DATEDIFF(day, DATEADD(day, 7, ReservationDate), GETDATE()) as 'Days left'
-		from Reservation 
-		where ClientID = @ClientID
-			and PaymentDate is null
-	)
+		return (
+			select c.BasePrice*(1-isnull(p.Discount, 0))*(1-c.StudentDiscount)
+			from Conferences as c
+				left outer join Prices as p
+					on p.ConferenceID = c.ConferenceID and 
+						(
+							select ReservationDate 
+							from Reservation 
+							where ReservationID = @reservationID
+						) between p.StartDate and p.EndDate	
+			where c.ConferenceID = (
+				select distinct cd.ConferenceID
+				from DayReservation as dr
+					inner join Reservation as r 
+						on r.ReservationID = dr.ReservationID
+					inner join	ConferenceDay as cd 
+						on cd.ConferenceDayID = dr.ConferenceDayID
+				where r.ReservationID = @reservationID
+			)
+		)
 	end
 go
+
+
+--wyswetlanie szczegolow rezerwacji
+create function function_reservationInfo(@reservationID int)
+	returns table
+	as
+	return (
+		select concat('Konferencja: ', c.ConferenceName, ', Data: ',
+			cd.ConferenceDate, ' - ', 'Liczba biletów normalnych: ', dr.NormalTickets)
+				as ReservationInfo
+		from DayReservation as dr
+			inner join ConferenceDay as cd
+				on cd.ConferenceDayID = dr.ConferenceDayID
+			inner join Conferences as c
+				on c.ConferenceID = cd.ConferenceID
+		where dr.ReservationID = @reservationID
+		union all
+		select concat('Konferencja: ', c.ConferenceName, ', Data: ',
+			cd.ConferenceDate, ' - ', 'Liczba biletów studenckich: ', dr.StudentTickets)
+				as ReservationInfo
+		from DayReservation as dr
+			inner join ConferenceDay as cd
+				on cd.ConferenceDayID = dr.ConferenceDayID
+			inner join Conferences as c
+				on c.ConferenceID = cd.ConferenceID
+		where dr.ReservationID = @reservationID
+		union all
+		select concat('Warsztat: ', wd.WorkshopName, ', Data: ',
+			cd.ConferenceDate, ' - ', 'Liczba biletów: ', wr.Tickets)
+				as ReservationInfo
+		from DayReservation as dr
+			inner join ConferenceDay as cd
+				on cd.ConferenceDayID = dr.ConferenceDayID
+			inner join WorkshopReservation as wr
+				on wr.DayReservationID = dr.DayReservationID
+			inner join Workshop as w
+				on w.WorkshopID = wr.WorkshopID
+			inner join WorkshopDictionary as wd
+				on wd.WorkshopDictionaryID = w.WorkshopDictionaryID
+			where dr.ReservationID = @reservationID
+        union all
+		select Concat('Cena: ',
+		    sum(dr.NormalTickets*dbo.function_returnNormalTicketCost(@reservationID))
+            +
+            sum(dr.StudentTickets*dbo.function_returnStudentTicketCost(@reservationID))
+		    +
+            sum(isnull(wr.Tickets*w.Price, 0))) as ReservationInfo
+        from DayReservation as dr
+            left outer join WorkshopReservation wr
+                    on dr.DayReservationID = wr.DayReservationID
+	        left outer join Workshop w
+	            on wr.WorkshopID = w.WorkshopID
+        where dr.ReservationID = @reservationID
+    )
+go
+
