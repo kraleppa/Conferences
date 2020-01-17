c2181b98cabc25d6ac66d618eb939a72a4e62c31
PC Windows <misiek.nalepa@wp.pl>
2020-01-17 21:30:05 +0100

    Company reservation is working

---
+++ CompanyReservation.sql
@@ -0,0 +1,145 @@
+create procedure procedure_addCompanyReservation
+    @ClientID int,
+    @ConferenceID int,
+    @DayList CompanyReservation READONLY,
+    @StudentList StudentIDCards READONLY
+as
+begin
+    set nocount on
+    begin try
+        begin tran addCompanyReservation
+            if (@ClientID is null or @ConferenceID is null)
+            begin
+                ;throw 52000, 'All arguments are compulsory', 1
+            end
+
+            if not exists(select * from Clients where ClientID = @ClientID)
+		    begin
+                ;throw 52000, 'Client does not exists', 1
+            end
+
+            if not exists(select * from Clients as c inner join Company C2 on c.ClientID = C2.ClientID where c.ClientID = @ClientID)
+		    begin
+                ;throw 52000, 'Client is not Company', 1
+            end
+
+            if not exists(select * from Conferences where ConferenceID = @ConferenceID)
+		    begin
+                ;throw 52000, 'Conference does not exists', 1
+            end
+
+            --sprawdzam czy student nie jest wpisany 2 razy na ten sam dzien
+            if exists(
+                select sl1.StudentIDCard, sl2.ConferenceDate, count(sl2.ConferenceDate)
+                from (select distinct StudentIDCard from @StudentList) as sl1
+                inner join @StudentList as sl2 on sl1.StudentIDCard = sl2.StudentIDCard
+                group by  sl1.StudentIDCard, sl2.ConferenceDate
+                having  count(sl2.ConferenceDate) <> 1
+            )
+            begin
+                ;throw 52000, 'Student is signed 2 times for one day', 1
+            end
+
+            declare @numberOfDays int = (select count(*) from @DayList);
+            if (@numberOfDays <= 0)
+            begin
+                ;throw 52000, 'DayList cannot be empty', 1
+            end
+
+            insert into Reservation(
+                ClientID,
+                ReservationDate
+            ) VALUES (@ClientID, GETDATE())
+            declare @ReservationID int = @@IDENTITY
+
+
+            declare @Date date;
+            declare @ConferenceDayID int;
+            declare @NormalTickets int;
+            declare @StudentTickets int;
+            declare @DayReservationID int;
+            declare @NumberOfStudents int = (select count(*) from @StudentList);
+            declare @StudentDate date;
+            declare @StudentIDCard  varchar(50);
+
+            declare @iterator1 int = 1;
+            declare @iterator2 int;
+            while (@iterator1 <= @numberOfDays)
+            begin
+                set @Date = (select ConferenceDate from @DayList where ID = @iterator1)
+                set @conferenceDayID = dbo.function_returnConferenceDay (@ConferenceID, @Date);
+                if (@ConferenceDayID is null)
+                begin
+                    ;throw 52000, 'Conference day does not exist', 1
+                end
+                set @NormalTickets = (select NormalTickets from @DayList where ID = @iterator1);
+                if (@NormalTickets <= 0)
+                begin
+                    ;throw 52000, 'Number of tickets is invalid', 1
+                end
+                set @studentTickets = (select count(*) from @StudentList where ConferenceDate = @date);
+/*wrocic*/      exec procedure_addCompanyDayReservation @ReservationID, @conferenceDayID, @normalTickets, @StudentTickets
+                set @DayReservationID = @@IDENTITY;
+
+                --dodaje studentow do danego dnia
+                if (@StudentTickets > 0)
+                begin
+                    set @iterator2 = 1;
+                    while(@iterator2 <= @NumberOfStudents)
+                    begin
+                        set @studentDate = (select ConferenceDate from @StudentList where ID = @iterator2);
+                        if not exists (select * from @DayList where ConferenceDate = @studentDate)
+                        begin
+                            ;throw 52000, 'Invalid student date', 1
+                        end
+                        set @StudentIDCard = (select StudentIDCard from @StudentList where ConferenceDate = @date and ID = @iterator2);
+                        exec procedure_initializeEmployee @ClientID,  @StudentIDCard
+                        set @iterator2 = @iterator2 + 1;
+                    end
+                end
+
+                set @iterator1 = @iterator1 + 1;
+            end
+
+        commit tran addCompanyReservation
+    end try
+    begin catch
+        rollback tran addCompanyReservation
+        declare @errorMessage nvarchar(2048)
+			= 'Cannot add Reservation. Error message: '+ error_message();
+		;throw 52000, @errorMessage, 1
+    end catch
+end
+go
+
+create procedure procedure_initializeEmployee
+    @ClientID int,
+    @StudentIDCard varchar(50)
+as
+begin
+    set nocount on
+    begin try
+        if (@ClientID is null)
+        begin
+            ;throw 52000, 'ClientID is compulsory', 1
+        end
+        insert Person default values
+        declare @PersonID int = @@IDENTITY
+        insert into Employee(ClientID, PersonID, FirstName, LastName)
+        VALUES(@ClientID, @PersonID, null, null)
+        if (@StudentIDCard is not null)
+        begin
+            if not exists(select * from Student where StudentCardID = @StudentIDCard)
+            begin
+                insert into Student(StudentCardID, PersonID)
+                Values (@StudentIDCard, @PersonID)
+            end
+        end
+    end try
+    begin catch
+        declare @errorMessage nvarchar(2048)
+			= 'Cannot initialize Employee. Error message: '+ error_message();
+		;throw 52000, @errorMessage, 1
+    end catch
+end
+
--- Querry.sql
+++ Querry.sql
@@ -1,87 +1,26 @@
-select * from Clients
-select * from Conferences
+exec procedure_addConference 'Edukacja', 'opis', '2021-07-20',
+    '2021-07-25',  'Krakow', 'Polska', 'Dluga',
+    '20', 0.1, 300, 50
 
-exec procedure_addConference 'Edukacja', 'opis', '2021-07-20', '2021-07-25', 'Krakow',
-    'Polska', 'Dluga', '2', 0.1, '300', 200
-
-select * from Conferences
-
-exec procedure_addWorkshopToDictionary 'Java', 'opis', '20'
-
-exec procedure_addWorkshop 1, '2021-07-20', 1, '08:00:00', '09:00:00', 40
-
-exec procedure_addWorkshop 1, '2021-07-21', 1, '08:00:00', '09:00:00', 40
-
-select * from Workshop inner join WorkshopDictionary WD on Workshop.WorkshopDictionaryID = WD.WorkshopDictionaryID
-
-exec procedure_addIndividualClient 'Krzysztof', 'Nalepa', '503748827', 'knalepa@gmail.com', '123456',
-    'Polska', 'Krakow', 'Wielopole', '10'
-
-select * from Clients inner join IndividualClient IC on Clients.ClientID = IC.ClientID
-
-declare @DayList1 IndividualReservation
-insert into @DayList1(ConferenceDate)VALUES('2021-07-20')
-insert into @DayList1(ConferenceDate)VALUES('2021-07-21')
-exec procedure_addIndividualReservation 1, 1, @DayList1
-
-select * from Reservation inner join DayReservation DR on Reservation.ReservationID = DR.ReservationID
-
-exec procedure_addWorkshopIndividualReservation 1, 1
-
-select * from WorkshopReservation inner join WorkshopParticipant WP on WorkshopReservation.WorkshopReservationID = WP.WorkshopReservationID
-    inner join DayParticipant DP on WP.DayParticipantID = DP.DayParticipantID
-
-
-exec procedure_addCompany 'AGHA', '666777929', '888782976', 'agh2@agh.edu', 'Polska', 'Krakow',
-    'Ulica', '1'
-
-select * from Company inner join Clients C on Company.ClientID = C.ClientID
-
-----------------
-declare @DayList2 CompanyReservation
-insert into @DayList2(ConferenceDate, NormalTickets)VALUES('2021-07-21', 1)
-insert into @DayList2(ConferenceDate, NormalTickets)VALUES('2021-07-22', 1)
-
-declare @StudentList StudentIDCards
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-21', '203302')
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-22', '203302')
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-21', '103302')
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-22', '203302')
-
-
-exec procedure_addCompanyReservation 3,  1, @DayList2, @StudentList
-
-select * from Reservation inner join DayReservation DR on Reservation.ReservationID = DR.ReservationID
-
-select * from Person inner join Employee E on Person.PersonID = E.PersonID left outer join Student S on Person.PersonID = S.PersonID
-
-declare @PersonIDList WorkshopReservation
-insert into @PersonIDList (PersonID)VALUES (10)
-exec procedure_addWorkshopCompanyReservation 3, 2, @PersonIDList
-
-
-select * from Reservation
-exec procedure_removeReservation 3
-
-exec procedure_addWorkshop 1, '2021-07-22', 1, '10:00:00', '11:00:00', 1000
-exec procedure_addWorkshop 1, '2021-07-22', 1, '9:00:00', '11:00:00', 1000
-select * from Workshop
+exec procedure_addCompany 'Awiteks', '203677888', '502758927',
+    'awiteks@gmail.com', 'Polska', 'Warszawa', 'Wiejska',
+    '1'
 
 declare @DayList CompanyReservation
-insert into @DayList(ConferenceDate, NormalTickets)VALUES('2021-07-22', 1)
+insert into @DayList(ConferenceDate, NormalTickets)VALUES('2021-07-20',  2)
+insert into @DayList(ConferenceDate, NormalTickets)VALUES('2021-07-21',  2)
 
 declare @StudentList StudentIDCards
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-22', '103302')
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-07-22', '203302')
-exec procedure_addCompanyReservation 4, 1, @DayList, @StudentList
+insert into @StudentList(ConferenceDate, StudentIDCard)VALUES('2021-07-20','305376')
+insert into @StudentList(ConferenceDate, StudentIDCard)VALUES('2021-07-21','305376')
 
-select * from Reservation
 
-select * from Workshop
 
-exec procedure_addWorkshop 1, '2021-07-22', 1, '09:00:00', '10:00:00', 800
 
-select * from Workshop
 
-select * from Company
 
+exec procedure_addCompanyReservation 1, 1, @DayList, @StudentList
+
+select * from Reservation inner join DayReservation DR on Reservation.ReservationID = DR.ReservationID
+
+select * from Student
