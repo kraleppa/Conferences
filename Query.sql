5e959f847a088b44a1fa9f3d79c668011b0378fc
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-01-18 19:45:35 +0100

    added generator fixes

--- Query.sql
+++ Query.sql
@@ -1,38 +1,18 @@
-select * from Clients
-select * from Conferences
+select * from function_reservationSummary(5)
 
-sele
+select * from IndividualClient as ic inner join person as p on ic.PersonID = p.PersonID
+order by 2
 
-exec procedure_addCompany '123056789', 'Wiejska', 'Kraków','Polska', '1', 'lawaa@wp.pl', '50201', 'xd'
-exec procedure_addCompany '606909872', 'Krótka', 'Warszawa', 'Polska', '22', 'autonalepa@onet.pl', '7845288771', 'Auto Nalepa'
-exec procedure_addCompany '510263473', 'Jana Pawła', 'Lublin', 'Polska', '30', 'ciastak@gmail.com', '999086023', 'Ciastka.pl'
-exec procedure_addCompany '666897698', 'Jana Pawła', 'Lublin', 'Polska', '29', 'torty@gmail.com', '923286023', 'Torty.pl'
-exec procedure_addCompany '6', 'Jana Pawła', 'Lublin', 'Polska', '29', 'rty@ml.com', '9202', 'T.l'
-select * from Company
-declare @DayList CompanyReservation;
-insert into @DayList (ConferenceDate, NormalTickets)VALUES('2021-09-10', 10)
-insert into @DayList (ConferenceDate, NormalTickets)VALUES ('2021-09-11', 2)
+select * from Employee as ic inner join person as p on ic.PersonID = p.PersonID
+order by 2
 
-declare @StudentList StudentIDCards;
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-09-01', '132')
-insert into @StudentList (ConferenceDate, StudentIDCard)VALUES ('2021-09-11', '33231276')
-exec procedure_addCompanyReservation 3, 1, @DayList, @StudentList
-select * from Reservation
-select * from DayReservation
-
-select * from Employee
-select * from Student
-select * from DayParticipant
-select * from Student
-
-select * from Reservation
-select * from DayReservation
-select * from Employee
+select * from Clients as c
+    inner join  IndividualClient on c.ClientID = IndividualClient.ClientID
+inner join Company as c2 on c.ClientID = c2.ClientID
+order by 1
 
 
 
-select * from DayParticipant
-select * from Reservation
-select * from DayReservation
-select * from DayParticipant
-select * from IndividualClient
+
+
+select * from ConferenceDay
